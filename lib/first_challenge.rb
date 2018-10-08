a53cb0471f3625dd5a681acd9e2e2d0a57801c77
Roberto Samia <bojosteph@gmail.com>
2018-10-08 22:04:52 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -5,7 +5,8 @@ def first_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => []
+      :hero_friends => [
+        ]
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
